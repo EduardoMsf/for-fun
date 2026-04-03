a03600ca99ddde394863da916bfdfa3a07a507e6
Eduardo Samaniego <samaniegomsf@gmail.com>
2026-04-03 14:08:17 -0600

    feat(app): add gradient side and product slug [JEMS-0304]

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,42 @@
-export default function ProductPage() {
+import { Title } from '@/src/components';
+import { initialData } from '@/src/seed/seed';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+interface Props {
+  params: Promise<{
+    slug: string;
+  }>;
+}
+
+export default async function ProductPage({ params }: Props) {
+  const { slug } = await params;
+
+  const product = initialData.products.find((product) => product.slug === slug);
+
+  if (!product) {
+    notFound();
+  }
   return (
-    <div>
-      <h1>Hello Product Page</h1>
+    <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+      {/* Image Section */}
+      <div className="col-span-1 md:col-span-2">
+        <Image
+          src={`/products/${product.images[0]}`}
+          alt={product.title}
+          width={500}
+          height={500}
+        />
+      </div>
+
+      <div className="col-span-1 px-5">
+        <h1 className="antialiased font-bold text-xl">{product.title}</h1>
+        <p className="text-2xl font-bold">${product.price.toFixed(2)}</p>
+
+        <button className="btn-primary my-5">Add to Cart</button>
+        <h3 className="font-bold text-sm">Description</h3>
+        <p className="font-light">{product.description}</p>
+      </div>
     </div>
   );
 }
---
+++ src/app/(shop)/product/not-found.tsx
@@ -0,0 +1,5 @@
+import { NotFound } from '@/src/components';
+
+export default function NotFoundProductPage() {
+  return <NotFound />;
+}
