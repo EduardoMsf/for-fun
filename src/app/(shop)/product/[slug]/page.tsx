87389fba94b4204c239db91d99b9905f7630a5fb
Eduardo Samaniego <samaniegomsf@gmail.com>
2026-04-03 14:53:54 -0600

    feat(components): add quantity selector [JEMS-0304]

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { Title } from '@/src/components';
+import { QuantitySelector, SizeSelector } from '@/src/components';
 import { initialData } from '@/src/seed/seed';
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
@@ -32,6 +32,13 @@ export default async function ProductPage({ params }: Props) {
       <div className="col-span-1 px-5">
         <h1 className="antialiased font-bold text-xl">{product.title}</h1>
         <p className="text-2xl font-bold">${product.price.toFixed(2)}</p>
+
+        <SizeSelector
+          selectedSize={product.sizes[0]}
+          availableSizes={product.sizes}
+        />
+
+        <QuantitySelector quantity={2} />
 
         <button className="btn-primary my-5">Add to Cart</button>
         <h3 className="font-bold text-sm">Description</h3>
